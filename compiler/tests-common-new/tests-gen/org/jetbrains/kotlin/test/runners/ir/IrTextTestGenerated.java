f53279b7905d636ce288a3aede467ee71a70cbf0
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2022-02-02 10:45:02 +0300

    ~~~ Copy IR text test from previous branch

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/ir/IrTextTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/ir/IrTextTestGenerated.java
@@ -2555,6 +2555,12 @@ public class IrTextTestGenerated extends AbstractIrTextTest {
         }
 
         @Test
+        @TestMetadata("kt48945a.kt")
+        public void testKt48945a() throws Exception {
+            runTest("compiler/testData/ir/irText/firProblems/kt48945a.kt");
+        }
+
+        @Test
         @TestMetadata("lambdaInEnumEntryConstructorCall.kt")
         public void testLambdaInEnumEntryConstructorCall() throws Exception {
             runTest("compiler/testData/ir/irText/firProblems/lambdaInEnumEntryConstructorCall.kt");
--- compiler/tests-gen/org/jetbrains/kotlin/klib/KlibTextTestCaseGenerated.java
+++ compiler/tests-gen/org/jetbrains/kotlin/klib/KlibTextTestCaseGenerated.java
@@ -1892,6 +1892,11 @@ public class KlibTextTestCaseGenerated extends AbstractKlibTextTestCase {
             runTest("compiler/testData/ir/irText/firProblems/kt48945.kt");
         }
 
+        @TestMetadata("kt48945a.kt")
+        public void testKt48945a() throws Exception {
+            runTest("compiler/testData/ir/irText/firProblems/kt48945a.kt");
+        }
+
         @TestMetadata("lambdaInEnumEntryConstructorCall.kt")
         public void testLambdaInEnumEntryConstructorCall() throws Exception {
             runTest("compiler/testData/ir/irText/firProblems/lambdaInEnumEntryConstructorCall.kt");
